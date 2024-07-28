3e26368c495b81d998fe2d1f546fe343c5579e87
Влад Кот <vanderofcat@gmail.com>
2024-07-28 11:30:51 +0300

    turns out this stuff were gitignored

---
+++ HotlineSPonyami/Gameplay/DebugStuff/DebugScene.cs
@@ -0,0 +1,27 @@
+﻿using NekoLib.Core;
+using NekoRay;
+using NekoRay.Physics2D;
+using ZeroElectric.Vinculum;
+using Camera2D = NekoRay.Camera2D;
+
+namespace HotlineSPonyami.Gameplay.DebugStuff; 
+
+public class DebugScene : BaseScene {
+    public override void Initialize() {
+        this.CreateWorld();
+        #region Camera
+        var camera = new GameObject("Camera").AddComponent<Camera2D>();
+        camera.BackgroundColor = new Color(203, 219, 252, 255);
+        camera.IsMain = true;
+        camera.Zoom = 2f;
+        #endregion
+
+        #region Reticle
+        var reticle = new GameObject("Reticle").AddComponent<ReticleController>();
+        var reticleRenderer = reticle.GameObject.AddComponent<SpriteRenderer2D>();
+        reticleRenderer.Sprite = new Sprite(Data.GetTexture("data/textures/gameui/reticle.png"), new Rectangle(0, 0, 9, 9));
+        #endregion
+        
+        base.Initialize();
+    }
+}
